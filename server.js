de7267904e214f12b9b213ba68b57dac284bf187
fco701115 <118506401+fco701115@users.noreply.github.com>
2026-07-29 15:00:29 -0600

    fix: set default port back to 3002

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(compression());
